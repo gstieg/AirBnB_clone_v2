f6fbf6c99267e2dd8c4a287cb20d0e700285005d
Grayson Stiegler <682@holbertonschool.com>
2019-08-14 05:01:54 +0000

    fix syntax

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,6 @@
--- create a MySQL server for the project hbnb_dev_db
--- a new user hbnb_dev
--- password hbnb_dev_pwd
--- all privileges
+-- script prepares mysql server for project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON 'hbnb_dev_db' .* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON 'performance_schema' .* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
+USE hbnb_dev_db;
+CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO hbnb_dev@localhost;
+GRANT SELECT ON performance_schema.* TO hbnb_dev@localhost;
