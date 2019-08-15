28910cb52f746b333df196a172e90d7ddfaf606e
Grayson Stiegler <682@holbertonschool.com>
2019-08-15 01:13:17 +0000

    add class attributes

--- models/user.py
+++ models/user.py
@@ -11,7 +11,8 @@ class User(BaseModel):
         first_name: first name
         last_name: last name
     """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
