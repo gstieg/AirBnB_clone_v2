0f0041b4feaf09f0ddd15df71ed0334ef65fa957
Grayson Stiegler <682@holbertonschool.com>
2019-08-15 18:29:56 +0000

    inherit base to class user

--- models/user.py
+++ models/user.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This is the class for user
     Attributes:
         email: email address
