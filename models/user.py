05ccb29626f60c64e356da9cd234c9426f618aa0
Grayson Stiegler <682@holbertonschool.com>
2019-08-16 06:14:50 +0000

    add class attributes

---
+++ models/user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Update User by adding attributes"""
+from models.base_model import BaseModel, Base
+
+
+class User(BaseModel, Base):
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+    """
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
