1a3a43de864b239170b213b14d2ff2a7145e84ac
Grayson Stiegler <682@holbertonschool.com>
2019-08-19 21:30:29 +0000

    sets up your web servers for the deployment

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# sets up your web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install nginx
+mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+printf "<html>
+<head>
+</head>
+<body>
+Holberton School
+</body>
+</html>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data
+sed -i '/server/a location /hbnb_static/ {alias /data/web_static/current/;}' /etc/nginx/sites/available/default
+service nginx restart
