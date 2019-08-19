284412e2fbdfd018e8e3c614188bab9140eaa3f7
Grayson Stiegler <682@holbertonschool.com>
2019-08-19 21:49:52 +0000

    re add web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,13 +6,14 @@ sudo apt-get install nginx
 mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 printf "<html>
-<head>
-</head>
-<body>
-Holberton School
-</body>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
 </html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data
-sed -i '/server/a location /hbnb_static/ {alias /data/web_static/current/;}' /etc/nginx/sites/available/default
+sed -i '/server/a location /hbnb_static {alias /data/web_static/current/;}' \
+    /etc/nginx/sites/available/default
 service nginx restart
