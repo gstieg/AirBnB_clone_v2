795d0fd763981487dcc9d3feadf6f968a851ca21
Grayson Stiegler <682@holbertonschool.com>
2019-08-19 22:13:48 +0000

    generates a .tgz archive

---
+++ 1-pack_web_static.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""generates a .tgz archive from the contents of the web_static"""
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """def do pack prototype"""
+    time = datetime.now().strftime('%Y%m%d$H%M%S')
+    local("mkdir -p versions")
+    path = local("tar -czvf versions/web_static_{}.tgz web_static".format(time))
+    if path.succeeded:
+        return path
+    else:
+        return(None)
