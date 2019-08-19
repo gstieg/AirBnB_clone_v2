cc66e606df178b3df732ec04ffc4472cd4d9d763
Grayson Stiegler <682@holbertonschool.com>
2019-08-19 23:02:35 +0000

    1-pack_web_static.py fix pep8

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,7 +8,8 @@ def do_pack():
     """def do pack prototype"""
     time = datetime.now().strftime('%Y%m%d$H%M%S')
     local("mkdir -p versions")
-    path = local("tar -czvf versions/web_static_{}.tgz web_static".format(time))
+    path = local("tar -czvf versions/web_static_{}.tgz web_static/"
+                 .format(time))
     if path.succeeded:
         return path
     else:
